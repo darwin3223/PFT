cb795a3900f83d343e6db0f07db6386b2ab1c92a
Damian.viera <damian.viera@estudiantes.utec.edu.uy>
2023-11-22 11:49:28 -0300

    Cambio en ver Reclamos

--- app/src/main/java/com/example/pft/FragmentVerReclamos.kt
+++ app/src/main/java/com/example/pft/FragmentVerReclamos.kt
@@ -56,7 +56,7 @@ class FragmentVerReclamos : Fragment() {
             val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, emptyListMessage)
             listViewReclamos.adapter = adapter
         } else {
-            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, reclamosFiltrados.map { it.titulo })
+            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, reclamosFiltrados.map { it.titulo+" - "+it.estudiante?.nombreUsuario })
             listViewReclamos.adapter = adapter
 
             if (mainActivity.usuarioLogueado?.tipoUsuario == "ESTUDIANTE"){
